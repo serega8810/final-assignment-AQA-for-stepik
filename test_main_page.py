1375bea3495b429f806e90b60a84a968f80616c8
serega88 <ctrannik8810@gmail.com>
2024-04-02 13:19:09 +0300

    adding check methods implementation of LoginPage

--- test_main_page.py
+++ test_main_page.py
@@ -1,4 +1,5 @@
 from pages.main_page import MainPage
+from pages.login_page import LoginPage
 
 
 def test_guest_can_go_to_login_page(browser):
@@ -6,3 +7,17 @@ def test_guest_can_go_to_login_page(browser):
     page = MainPage(browser, link)      # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
     page.open()                         # открываем страницу
     page.go_to_login_page()             # выполняем метод страницы — переходим на страницу логина
+
+
+def test_guest_should_see_login_link(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open()
+    page.should_be_login_link()
+
+
+def test_should_see_login_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/accounts/login/"
+    page = LoginPage(browser, link)
+    page.open()
+    page.should_be_login_page()
