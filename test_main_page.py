99050a19fe83b4eb76cf126344228ee81b61824a
serega88 <ctrannik8810@gmail.com>
2024-04-04 10:35:32 +0300

    adding checks for adding goods to cart

--- test_main_page.py
+++ test_main_page.py
@@ -1,5 +1,9 @@
+import time
+
 from pages.main_page import MainPage
 from pages.login_page import LoginPage
+from pages.product_page import ProductPage
+from pages.product_page import BasePage
 
 
 def test_guest_can_go_to_login_page(browser):
@@ -16,8 +20,31 @@ def test_guest_should_see_login_link(browser):
     page.should_be_login_link()
 
 
-def test_should_see_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/en-gb/accounts/login/"
-    page = LoginPage(browser, link)
+def test_guest_can_go_to_login_page(browser):
+    link = "http://selenium1py.pythonanywhere.com"
+    page = MainPage(browser, link)
     page.open()
-    page.should_be_login_page()
+    page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
+
+
+def test_guest_can_add_product_to_basket(browser):
+    link = "https://selenium1py.pythonanywhere.com/en-gb/catalogue/the-shellcoders-handbook_209/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.add_product_to_basket()
+    page.check_product_added()
+    page.check_cost_product_added()
+
+
+def test_solve_quiz_and_get_code(browser):
+    link = "https://selenium1py.pythonanywhere.com/en-gb/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+    page = BasePage(browser, link)
+    page1 = ProductPage(browser, link)
+    page.open()
+    page1.add_product_to_basket()
+    page.solve_quiz_and_get_code()
+
+
+
