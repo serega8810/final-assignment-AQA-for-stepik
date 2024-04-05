23774616cd0e40d9a507207e2e81d6cef6a28fe7
serega88 <ctrannik8810@gmail.com>
2024-04-05 12:52:08 +0300

    adding negative checks

--- test_main_page.py
+++ test_main_page.py
@@ -50,3 +50,27 @@ def test_guest_can_add_product_to_basket(browser, link):
     page.check_product_added()
     page.check_cost_product_added()
 
+@pytest.mark.skip(reason="message on site")
+def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.add_product_to_basket()
+    page.should_not_be_success_message()    # ожидаем, что там нет сообщения об успешном добавлении в корзину
+
+
+def test_guest_cant_see_success_message(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_not_be_success_message()  # ожидаем, что там нет сообщения об успешном добавлении в корзину
+
+
+@pytest.mark.xfail(reason="the message is in place and does not disappear")
+def test_message_disappeared_after_adding_product_to_basket(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.add_product_to_basket()
+    page.success_message_should_disappear()  # элемент присутствует на странице и должен исчезнуть
+
